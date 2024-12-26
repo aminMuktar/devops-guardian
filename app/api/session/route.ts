dfe33100a655769b3ec6dafc51facb614fc8d2f8
aminMuktar <aminmuktar41@gmail.com>
2024-12-26 09:34:05 +0300

    add: request params

--- app/api/session/route.ts
+++ app/api/session/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 
 
 
-export async function GET(req: Request) {
+export async function GET() {
     const session = await getServerSession(authOptions);
 
     if(!session) {
